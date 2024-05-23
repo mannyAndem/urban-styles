20a534c2a0e686858f59cb981175498f64875b7e
Emmanuel Andem <emmanuelandem360@gmail.com>
2024-05-23 13:46:31 +0100

    chore: update config

--- medusa-backend/medusa-config.js
+++ medusa-backend/medusa-config.js
@@ -2,16 +2,16 @@ const dotenv = require("dotenv");
 
 let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
-  // case "production":
-  //   ENV_FILE_NAME = ".env.production";
-  //   break;
-  // case "staging":
-  //   ENV_FILE_NAME = ".env.staging";
-  //   break;
-  // case "test":
-  //   ENV_FILE_NAME = ".env.test";
-  //   break;
-  // case "development":
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
   default:
     ENV_FILE_NAME = ".env";
     break;
@@ -65,20 +65,20 @@ const plugins = [
   },
 ];
 
-// const modules = {
-//   eventBus: {
-//     resolve: "@medusajs/event-bus-redis",
-//     options: {
-//       redisUrl: REDIS_URL,
-//     },
-//   },
-//   cacheService: {
-//     resolve: "@medusajs/cache-redis",
-//     options: {
-//       redisUrl: REDIS_URL,
-//     },
-//   },
-// };
+const modules = {
+  eventBus: {
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: REDIS_URL,
+    },
+  },
+  cacheService: {
+    resolve: "@medusajs/cache-redis",
+    options: {
+      redisUrl: REDIS_URL,
+    },
+  },
+};
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
@@ -88,12 +88,12 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL,
+  redis_url: REDIS_URL,
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
   projectConfig,
   plugins,
-  // modules,
+  modules,
 };
